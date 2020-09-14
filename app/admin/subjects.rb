133a5393d0c0be67a70c26a54a83ac189d49126b
allezz <aerbay@gmail.com>
2020-09-15 01:04:02 +0200

    Add custom show page in subject

--- app/admin/subjects.rb
+++ app/admin/subjects.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Subject do
   permit_params :name, note_ids: []
+
+  show title: 'My Title' do
+    h3 'You have ' + pluralize(subject.notes.count, 'note') + ' for this subject'
+    subject.notes.each do |note|
+      div do
+        h1 note.title
+        h4 link_to "[#{note.id}]: You created this note on #{note.created_at.strf('%a %b %e, %Y at %I%M %p')}", admin_note_path(note)
+      end
+    end
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
